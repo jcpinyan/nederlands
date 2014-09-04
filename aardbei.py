a7acd7f34a3bcf5f9d03c7ed3390f7f8d057d0e5
jcpinyan <jcpinyan@users.noreply.github.com>
2014-09-04 17:01:37 -0400

    skeleton of what the script has to do

---
+++ aardbei.py
@@ -0,0 +1,10 @@
+# /usr/bin/end python3
+
+# I/O from user
+# read in and parse sentences
+# read in and parse definitions
+# assemble woord objects
+# make a multiple choice question
+# deliver to user
+## accept user choice
+## judge and return 
