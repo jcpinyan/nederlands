ea5adc5e4f763c7cb784a6dbb64b7d8cfbbf540c
Jennifer VB <jcpinyan@users.noreply.github.com>
2014-09-06 17:26:33 -0400

    read in defs too

--- aardbei.py
+++ aardbei.py
@@ -7,6 +7,12 @@ filename = 'sentences'
 with open(filename) as f:
     sentences = f.readlines()
 
+filename = 'definitions'
+with open(filename) as f:
+    definitions = f.readlines()
+
+
+
 # read in and parse definitions
 # assemble woord objects
 # make a multiple choice question
