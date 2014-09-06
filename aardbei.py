0618d8c6aaf5c6d7c8691186cf9f52d7df7d06b0
Dan Van Boxel <the.dvb@gmail.com>
2014-09-06 10:37:45 -0400

    Add primitive woord class

--- aardbei.py
+++ aardbei.py
@@ -14,7 +14,27 @@ with open(filename) as f:
 
 
 # read in and parse definitions
+
 # assemble woord objects
+class woord:
+    def __init__(self,nederlands, engels, zin, tags = None, gender = None):
+        self.nederlands = nederlands
+        self.engels = engels
+        self.zin = zin
+        self.tags = tags
+        self.gender = gender
+    
+    def __repr__(self):
+        return 'woord({0},{1},{2},{3},{4})'.format(self.nederlands,
+                                                   self.engels,
+                                                   self.zin,
+                                                   self.tags,
+                                                   self.gender)
+
+    def __str__(self):
+        return self.nederlands
+
+
 # make a multiple choice question
 # deliver to user
 ## accept user choice
