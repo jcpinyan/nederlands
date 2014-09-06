0bb8cbf4fdbaac70acce8e27de8d422d86fc1c00
Jennifer VB <jcpinyan@users.noreply.github.com>
2014-09-05 22:31:13 -0400

    read in sentences

--- aardbei.py
+++ aardbei.py
@@ -2,6 +2,11 @@
 
 # I/O from user
 # read in and parse sentences
+
+filename = 'sentences'
+with open(filename) as f:
+    sentences = f.readlines()
+
 # read in and parse definitions
 # assemble woord objects
 # make a multiple choice question
